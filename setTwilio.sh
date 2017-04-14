c62d8e4c51dafb15ddc186d1d0616f57d9dd60a2
Srini Karlekar <skarlekar@yahoo.com>
2017-04-13 20:25:39 -0400

    Twilio environment variables

---
+++ setTwilio.sh
@@ -0,0 +1,4 @@
+export TWILIO_ACCOUNT_SID=your Twilio SID goes here
+export TWILIO_AUTH_TOKEN=your Twilio Authorization token goes here
+export TWILIO_TO_NUMBER=your personal mobile number goes here
+export TWILIO_FROM_NUMBER=your Twilio number goes here
