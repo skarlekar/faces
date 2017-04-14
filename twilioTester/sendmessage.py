9cc4fbfd674034768ab333a699a03ab2309ac654
Srini Karlekar <skarlekar@yahoo.com>
2017-04-13 23:25:14 -0400

    Added current time to Twilio Tester

--- twilioTester/sendmessage.py
+++ twilioTester/sendmessage.py
@@ -1,5 +1,6 @@
 from twilio.rest import TwilioRestClient
 import os 
+import datetime
 
 def getTwilioCredentials():
     SID = os.environ['TWILIO_ACCOUNT_SID']
@@ -14,10 +15,11 @@ def getTwilioFromTo():
 ACCOUNT_SID, AUTH_TOKEN = getTwilioCredentials()
 twilio_client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 fromNum, toNum = getTwilioFromTo()
+date_time = str(datetime.datetime.now())
 
 twilio_client.messages.create(
     to = toNum,
     from_= fromNum,
-    body="This image of Neil Armstrong is the most influential image of our time. A vulnerable human on a distant world - a world that would kill him if he removed so much as a single article of his exceedingly complex clothing.",
+    body="This image of Neil Armstrong is the most influential image of our time. A vulnerable human on a distant world - a world that would kill him if he removed so much as a single article of his exceedingly complex clothing. Current time is {}".format(date_time),
     media_url="http://dujye7n3e5wjl.cloudfront.net/photographs/1080-tall/time-100-influential-photos-neil-armstrong-nasa-man-moon-64.jpg",
 )
