e397ce7f722183391234f99753c47980e6a184c7
Srini Karlekar <skarlekar@yahoo.com>
2017-05-05 13:36:01 -0400

    setTwilio for Windows users

---
+++ setTwilio.cmd
@@ -0,0 +1,4 @@
+set TWILIO_ACCOUNT_SID=“your Twilio SID goes here”
+set TWILIO_AUTH_TOKEN=“your Twilio Authorization token goes here”
+set TWILIO_TO_NUMBER=“your personal mobile number goes here”
+set TWILIO_FROM_NUMBER=“your Twilio number goes here”
